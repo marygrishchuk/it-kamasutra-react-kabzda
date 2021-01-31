06aff3958a1f41cb6ff1aa6a66ca6eed0ac7a3ea
Maria Grishchuk <mary.grishchuk1@gmail.com>
2021-01-31 18:02:31 +0300

    useEffect with setTimeout and setInterval has been used (21)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -30,3 +30,53 @@ export const SimpleExample = () => {
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 }
+
+export const SetTimeoutExample = () => {
+    console.log("SetTimeoutExample")
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log("setTimeout")
+            document.title = counter.toString()
+        }, 1000)
+    }, [counter])
+
+    return <>
+        Hello, fake: {fake} counter: {counter}
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+}
+
+export const SetIntervalExample = () => {
+    console.log("SetIntervalExample")
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+    return <>
+        Hello, fake: {fake} counter: {counter}
+    </>
+}
+
+export const SetIntervalClockExample = () => {
+    console.log("SetIntervalClockExample")
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(new Date().toLocaleTimeString())
+        }, 1000)
+    }, [])
+
+    return <>
+        {time}
+    </>
+}
