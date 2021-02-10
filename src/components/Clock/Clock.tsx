7417f55b684e30e43f16be7aa61be28cd168703a
Maria Grishchuk <mary.grishchuk1@gmail.com>
2021-02-10 15:21:12 +0300

    Analog Clock has been added (22)

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,13 +1,16 @@
 import React, {useEffect, useState} from "react";
+import s from "./Clock.module.css"
 
-type PropsType = {
-}
+type PropsType = {}
 
 const getTwoDigitString = (num: number) => num < 10 ? "0" + num : num //time util
+const getMinSecDegree = (num: number) => num === 0 ? 90 : ((360 / 60) * num) + 90 //adding extra 90 degrees for transform
+const getHoursDegree = (num: number) => num === 0 ? 90 : ((360 / 24) * num * 2) + 90 //adding extra 90 degrees for transform
 
 export const Clock: React.FC<PropsType> = (props) => {
 
     const [date, setDate] = useState(new Date())
+    const [analogMode, setAnalogMode] = useState(false)
 
     useEffect(() => {
         const intervalID = setInterval(() => {
@@ -20,11 +23,26 @@ export const Clock: React.FC<PropsType> = (props) => {
         }
     }, [])
 
-    return <div>
-        <span>{getTwoDigitString(date.getHours())}</span>
-        :
-        <span>{getTwoDigitString(date.getMinutes())}</span>
-        :
-        <span>{getTwoDigitString(date.getSeconds())}</span>
-    </div>
+    return <>
+        <button disabled={!analogMode} onClick={() => setAnalogMode(false)}>Digital Clock</button>
+        <button disabled={!!analogMode} onClick={() => setAnalogMode(true)}>Analogue Clock</button>
+        {analogMode
+            ? <div className={s.clock}>
+                <div className={s.clockFace}>
+                    <div className={s.hand}
+                         style={{transform: `rotate(${getHoursDegree(date.getHours())}deg)`}}></div>
+                    <div className={s.hand}
+                         style={{transform: `rotate(${getMinSecDegree(date.getMinutes())}deg)`}}></div>
+                    <div className={s.hand}
+                         style={{transform: `rotate(${getMinSecDegree(date.getSeconds())}deg)`}}></div>
+                </div>
+            </div>
+            : <div>
+                <span>{getTwoDigitString(date.getHours())}</span>
+                :
+                <span>{getTwoDigitString(date.getMinutes())}</span>
+                :
+                <span>{getTwoDigitString(date.getSeconds())}</span>
+            </div>}
+    </>
 }
